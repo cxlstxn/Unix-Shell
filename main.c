11304e0025329c6246f53c074fab7ff72fb99db7
cxb23151 <finlay.colston.2023@uni.strath.ac.uk>
2025-01-31 18:14:27 +0000

    stage 1 done

--- main.c
+++ main.c
@@ -4,16 +4,34 @@
 
 int main() {
     int x = 1; 
-    while (x==1) {
+    while (x == 1) {
         char cwd[1024];
         getcwd(cwd, sizeof(cwd));
         printf("%s>$", cwd);
         char result[100]; 
         fgets(result, sizeof(result), stdin);
+        
+        if (feof(stdin)) {
+            break;
+        }
+        if (strcmp(result, "exit\n") == 0) {
+            break;
+        }
+
+        char* tokens[100];
+        int token_count = 0;
         char* token = strtok(result, " < \t | > & ;");
         while (token != NULL) {
-            printf(" %s\n", token);
+            char *newline = strchr(token, '\n');
+            if (newline) {
+            *newline = '\0';
+            }
+            tokens[token_count++] = token;
             token = strtok(NULL, " < \t | > & ;");
+        }
+        
+        for (int i = 0; i < token_count; i++) {
+            printf("'%s'\n", tokens[i]);
         }
     }
 }
