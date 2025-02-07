1e6b34b24a249aa1dd132b9fc448ed9a1315f3bb
Fraser Watt CS2023 <fraser.watt.2023@uni.strath.ac.uk>
2025-02-07 09:46:02 +0000

    Started Stage 2 - messy, WIP

--- main.c
+++ main.c
@@ -2,14 +2,22 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <unistd.h>
 #include <sys/wait.h>
 
 int main() {
-    while (1) {
+
+  // DO WHILE SHELL HAS NOT TERMINATED:
+    int x = 1; 
+    while (x == 1) {
         char cwd[1024];
-        getcwd(cwd, sizeof(cwd)); // gets working directory 
+        getcwd(cwd, sizeof(cwd)); // gets working directory
+
+	// DISPLAY PROMPT:
         printf("%s>$", cwd); // command line 
-        char userinput[100]; 
+        char userinput[100]; // changing this to char* helps execvp() but fucks fgets() 
+
+	// READ AND PARSE USER INPUT
         fgets(userinput, sizeof(userinput), stdin); // getting user input
         
 		// Checking for built in shell functions:
@@ -20,13 +28,13 @@ int main() {
             break;
         }
 		// exit check 
-        if (strcmp(userinput, "exit\n") == 0) { // exit -> exit program 
+        if (strcmp(userinput, "exit\n") == 0) { //exit -> exit program 
             break;
         }
 
         char* tokens[100];
         int token_count = 0;
-        char* token = strtok(result, " < \t | > & ;"); // tokens to look for
+        char* token = strtok(userinput, " < \t | > & ;"); // tokens to look for
         while (token != NULL) {
             char *newline = strchr(token, '\n');
             if (newline) {
@@ -35,16 +43,15 @@ int main() {
             tokens[token_count++] = token;
             token = strtok(NULL, " < \t | > & ;");
         }
-        
-		/* Stage 1 Testing
-        for (int i = 0; i < token_count; i++) {
-            printf("'%s'\n", tokens[i]); // loop through tokens printing them 
-        }
-		*/
+	/*
+	  Section to be executed by internal commands
+	*/ 
+        //else{ -- Section for external commands - can't be implemented until we have internals, obviously
 		
 		// Else - stage 2:
+	// ELSE EXECUTE COMMAND AS AN EXTERNAL PROCESS
 		
-		pid_t = pid;
+		pid_t pid;
 		pid = fork();
 		
 		if(pid<0){ // negative pid -> failure
@@ -52,15 +59,18 @@ int main() {
 			return 1;
 		}
 		else if(pid==0){ // signifies child process
-			execlp("/bin/ls","ls",NULL); // if fails - not our fault - OS problem
+		  //execlp("/bin/ls","ls",NULL); // if fails - not our fault - OS problem
+		  execvp(userinput[0], userinput); // passes user input[0] (function) and rest of string as arguement
+		  fprintf(stderr, "Error executing command!\n"); // shouldn't ever reach this part?
+		  return 1;
 			// only reached if exec fils - exit child process here:
 		}
 		else{
 			wait(NULL); // Parent waiting for child process to complete
 		}
-		
-    }
 	
+    }
+       
 }
 
 /* Notes:
