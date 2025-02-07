8fb21e57333b9f73ad560db1effc28c65dc0af9a
Fraser Watt CS2023 <gwb23145@cpunode3.cis.strath.ac.uk>
2025-02-07 16:05:16 +0000

    tokens -> tokenList + exit troubleshooting

--- main.c
+++ main.c
@@ -14,14 +14,14 @@ int main() {
 
 
     /* DISPLAY PROMPT: */
-    printf("%s>$", cwd); // command line 
+    printf("%s>$ ", cwd); // command line 
     char userinput[100];
 
 
     /* READ AND PARSE USER INPUT: */
     fgets(userinput, sizeof(userinput), stdin); // getting user input
 
-    char* tokens[100];
+    char* tokenList[100];
     int token_count = 0;
     char* token = strtok(userinput, " < \t | > & ;"); // tokens to look for
     while (token != NULL) {
@@ -29,25 +29,25 @@ int main() {
       if (newline) {
 	*newline = '\0'; // setting NULL position
       }
-      tokens[token_count++] = token;
+      tokenList[token_count++] = token;
       token = strtok(NULL, " < \t | > & ;");
     }
-    tokens[token_count++] = NULL; //last has to be null for execvp to work
+    tokenList[token_count++] = NULL; //last has to be null for execvp to work
 
 
     /* IF COMMAND IS BUILT-IN INVOKE APPROPRIATE FUNCTIONS: */
 
-    // Ctrl+d check
+  // Ctrl+d check
     if (feof(stdin)) { // ctrl+d -> exit program
       printf("\n");
       break;
     }
-	
+
+    
     // exit check 
     if (strcmp(userinput, "exit\n") == 0) { //exit -> exit program 
       break;
     }
-
     
     /* ELSE EXECUTE COMMAND AS AN EXTERNAL PROCESS: */
 
@@ -59,7 +59,7 @@ int main() {
       return 1;
     }
     else if(pid==0){ // signifies child process
-      execvp(tokens[0],tokens); // passes user input[0] (function) and rest of string as arguement
+      execvp(tokenList[0],tokenList); // passes user input[0] (function) and rest of string as arguement
       fprintf(stderr, "Error executing command!\n"); // shouldn't ever reach this part - reach it when command invlaid - WHY?
       return 1;
     }
